abf6dfd8ec45c9dbd9426613407e2c77cb5be380
Suzy Burgum <suzyburgum@gmail.com>
2024-04-25 08:36:35 -0400

    create LoginForm with FlaskForm

--- forms.py
+++ forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
+from wtforms import StringField, SubmitField, PasswordField
 from wtforms.validators import DataRequired, URL
 from flask_ckeditor import CKEditorField
 
@@ -22,6 +22,9 @@ class RegisterForm(FlaskForm):
 
 
 # TODO: Create a LoginForm to login existing users
-
+class LoginForm(FlaskForm):
+    email = StringField("Email", validators=[DataRequired()])
+    password = PasswordField("Password", validators=[DataRequired()])
+    submit = SubmitField("Let Me In!")
 
 # TODO: Create a CommentForm so users can leave comments below posts
